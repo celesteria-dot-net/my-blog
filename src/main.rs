aa550789ec2fe05c0362a4a6b72ef411fc8b32eb
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-04-07 00:14:31 +0900

    fix: fix ip for docker

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use actix_web::{App, HttpServer};
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().service(fs::Files::new("/", "./static").show_files_listing()))
-        .bind(("127.0.0.1", 8080))?
+        .bind(("0.0.0.0", 8080))?
         .run()
         .await
 }
