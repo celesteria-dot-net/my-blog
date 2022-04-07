51e643f104de83e0643a5f233761aa83c05d34e3
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-04-07 14:39:18 +0900

    feat: add logger and port config

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,46 @@
 use actix_files as fs;
-use actix_web::{App, HttpServer};
+use actix_web::{middleware::Logger, App, HttpServer};
+use log::{debug, info, warn};
+use std::env;
+
+const PORT_KEY: &str = "PORT";
+const DEFAULT_PORT: u16 = 8080;
+
+fn load_port() -> u16 {
+    let default_port_msg = format!("Default port ({}) will be used.", DEFAULT_PORT);
+
+    env::var(PORT_KEY)
+        .unwrap_or_else(|_| {
+            warn!(
+                "\"{}\" is not defined in environment variables. {}",
+                PORT_KEY, default_port_msg
+            );
+            DEFAULT_PORT.to_string()
+        })
+        .parse::<u16>()
+        .unwrap_or_else(|_| {
+            warn!("The port number is invalid. {}", default_port_msg);
+            DEFAULT_PORT
+        })
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(fs::Files::new("/", "./static").show_files_listing()))
-        .bind(("0.0.0.0", 8080))?
-        .run()
-        .await
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    info!("The server is starting!");
+    debug!("build server");
+    let server = HttpServer::new(|| {
+        App::new()
+            .wrap(Logger::default())
+            .service(fs::Files::new("/", "./static").show_files_listing())
+    });
+
+    debug!("bind port");
+    let port = load_port();
+    server.bind(("0.0.0.0", port))?.run().await?;
+
+    info!("The server has stopped!");
+
+    Ok(())
 }
