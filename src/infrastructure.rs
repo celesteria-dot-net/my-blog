7a76f0f606a8d474f25dc821ac263d61e3e6c82c
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-05-28 00:03:03 +0900

    feat: impl Repository#find_by_id

--- src/infrastructure.rs
+++ src/infrastructure.rs
@@ -2,14 +2,22 @@ use crate::domain::{model::*, repository::*};
 
 use derive_new::new;
 use gray_matter::{engine::YAML, Matter};
-use std::fs;
+use std::{fs, path::PathBuf};
 
 #[derive(new)]
 pub struct PostRepositoryImpl {}
 
 impl PostRepository for PostRepositoryImpl {
-    fn find_by_id(&self, _id: PostId) -> anyhow::Result<Post> {
-        Ok(Post::default())
+    fn find_by_id(&self, id: PostId) -> anyhow::Result<Post> {
+        let path = PathBuf::from(format!("./static/{}.md", id.0));
+        anyhow::ensure!(path.exists(), "The file is not found.");
+        let str = fs::read_to_string(path)?;
+        let matter = Matter::<YAML>::new();
+        let meta = matter
+            .parse_with_struct::<PostMeta>(&str)
+            .ok_or_else(|| anyhow::anyhow!("Failed to parse markdown."))?;
+
+        Ok(Post::new(id, meta.content, meta.data))
     }
 
     fn list(&self) -> anyhow::Result<Vec<Post>> {
