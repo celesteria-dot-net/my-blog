f4a306adb4ccc7706b5598180e3faca213bb9f52
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-05-27 23:41:45 +0900

    fix: build model from protobuf

--- build.rs
+++ build.rs
@@ -4,6 +4,4 @@
 fn main() {
     let json = include_str!("./build_config.json");
     prost_serde::build_with_serde(json);
-    // prost_build::compile_protos(&["post_meta.proto", "post.proto"], &["protobuf/my-blog/v1"])
-    //     .unwrap();
 }
