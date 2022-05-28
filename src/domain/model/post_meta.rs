0728d0355300e0f00254abbb09c1f8ab9b7d11ef
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-05-28 17:17:44 +0900

    test: add tests for PostMeta

--- src/domain/model/post_meta.rs
+++ src/domain/model/post_meta.rs
@@ -2,7 +2,7 @@ use chrono::NaiveDate;
 use derive_new::new;
 use serde::Deserialize;
 
-#[derive(new, Deserialize)]
+#[derive(new, Deserialize, PartialEq, Debug, Clone)]
 pub struct PostMeta {
     pub title: String,
     pub description: Option<String>,
@@ -66,5 +66,85 @@ mod serde_option_naive_date {
             }
             None => None,
         })
+    }
+}
+
+#[cfg(test)]
+mod deserialize_test {
+    use super::*;
+
+    use chrono::NaiveDate;
+    use gray_matter::{engine::YAML, Matter, ParsedEntityStruct};
+    use indoc::formatdoc;
+    use serde::de::DeserializeOwned;
+
+    fn deserialize_with<T: DeserializeOwned>(str: &str) -> Option<ParsedEntityStruct<T>> {
+        let matter = Matter::<YAML>::new();
+
+        matter.parse_with_struct::<T>(str)
+    }
+
+    fn deserialize(str: &str) -> Option<ParsedEntityStruct<PostMeta>> {
+        deserialize_with::<PostMeta>(str)
+    }
+
+    #[test]
+    fn only_required_fields() {
+        let expected = PostMeta {
+            title: "title".to_string(),
+            created_date: NaiveDate::from_ymd(2020, 1, 1),
+            ..PostMeta::default()
+        };
+        let str = formatdoc! {"
+            ---
+            title: {title}
+            created_date: {created_date}
+            ---
+            content!
+            ",
+            title = expected.title,
+            created_date = expected.created_date.format(FORMAT)
+        };
+        let found = deserialize(&str);
+
+        assert_eq!(found.map(|d| d.data), Some(expected));
+    }
+
+    #[test]
+    fn all_fields() {
+        let expected = PostMeta::new(
+            "title".to_string(),
+            Some("description".to_string()),
+            Some("image".to_string()),
+            Some(
+                vec!["image1", "image2"]
+                    .iter()
+                    .map(|s| s.to_string())
+                    .collect(),
+            ),
+            NaiveDate::from_ymd(2020, 1, 1),
+            Some(NaiveDate::from_ymd(2020, 1, 1)),
+        );
+        let str = formatdoc! { "
+                ---
+                title: {title}
+                description: {description}
+                image: {image}
+                tags: {tags:?}
+                created_date: {created_date}
+                updated_date: {updated_date}
+                ---
+                content!
+            ",
+            title = expected.title,
+            description = expected.clone().description.unwrap(),
+            image = expected.clone().image.unwrap(),
+            tags = expected.clone().tags.unwrap(),
+            created_date = expected.created_date.format(FORMAT),
+            updated_date = expected.updated_date.unwrap().format(FORMAT)
+        };
+        let found = deserialize(&str);
+
+        assert_eq!(found.map(|d| d.data), Some(expected));
     }
 }
